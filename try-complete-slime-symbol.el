399e3d802daf17230915741d179a0829af545d95
kurohuku <kurohuku@phorni.net>
2011-06-18 02:18:57 +0900

    ignore when (slime-connected-p) return nil

--- try-complete-slime-symbol.el
+++ try-complete-slime-symbol.el
@@ -2,33 +2,34 @@
 (require 'cl)
 
 (defun try-complete-slime-symbol (old)
-  (unless old
-    (he-init-string (he-lisp-symbol-beg) (point))
-    (unless (he-string-member he-search-string he-tried-table)
-      (setq he-tried-table (cons he-search-string he-tried-table)))
-    (setq he-expand-list
-	  (and (not (equal he-search-string ""))
-	       (sort
-		(case slime-complete-symbol-function
-		  ((slime-simple-complete-symbol)
-		   (get-completions/slime-simple-complete he-search-string))
-		  ((slime-fuzzy-completions)
-		   (get-completions/slime-fuzzy-complete-symbol he-search-string))
-		  ((slime-complete-symbol*)
-		   (get-completions/slime-complete-symbol*))
-		  (t (error "unexpected slime-complete-symbol-function")))
-		'string-lessp))))
-  (while (and he-expand-list
-	      (he-string-member (car he-expand-list) he-tried-table))
-    (setq he-expand-list (cdr he-expand-list)))
-  (if (null he-expand-list)
-      (progn
-	(when old (he-reset-string))
-	nil)
+  (when (slime-connected-p)
+    (unless old
+      (he-init-string (he-lisp-symbol-beg) (point))
+      (unless (he-string-member he-search-string he-tried-table)
+	(setq he-tried-table (cons he-search-string he-tried-table)))
+      (setq he-expand-list
+	    (and (not (equal he-search-string ""))
+		 (sort
+		  (case slime-complete-symbol-function
+		    ((slime-simple-complete-symbol)
+		     (get-completions/slime-simple-complete he-search-string))
+		    ((slime-fuzzy-completions)
+		     (get-completions/slime-fuzzy-complete-symbol he-search-string))
+		    ((slime-complete-symbol*)
+		     (get-completions/slime-complete-symbol*))
+		    (t (error "unexpected slime-complete-symbol-function")))
+		  'string-lessp))))
+    (while (and he-expand-list
+		(he-string-member (car he-expand-list) he-tried-table))
+      (setq he-expand-list (cdr he-expand-list)))
+    (if (null he-expand-list)
+	(progn
+	  (when old (he-reset-string))
+	  nil)
       (progn
 	(he-substitute-string (car he-expand-list))
 	(setq he-expand-list (cdr he-expand-list))
-	t)))
+	t))))
 
 (defun get-completions/slime-simple-complete-symbol (prefix)
   (car (slime-simple-completions prefix)))
